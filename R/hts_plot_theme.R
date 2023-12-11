88382fb2fe39734f9e3e5be09e99803f90010032
James Clark <james.clark@rsginc.com>
2023-12-11 10:52:26 -0700

    remove hts_reporting packages from description

--- R/hts_plot_theme.R
+++
@@ -1,6 +0,0 @@
-hts_plot_theme = ggplot2::theme(plot.background = ggplot2::element_rect(fill = "white"),  
-                                panel.background = ggplot2::element_rect(fill = "white"),
-                                plot.caption = ggplot2::element_text(hjust = 0, face= "italic"), 
-                                #plot.title.position = "plot", 
-                                #plot.caption.position =  "plot",
-                                axis.text = element_text(size = 10))
