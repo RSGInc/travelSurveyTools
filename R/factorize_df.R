0fe7c3a72980933a55e3a598770ed271cd8d0079
James Clark <james.clark@rsginc.com>
2024-02-05 15:47:37 -0700

    shorten message

--- R/factorize_df.R
+++ R/factorize_df.R
@@ -54,10 +54,10 @@ factorize_df <- function(df, vals_df, verbose = TRUE, ...) {
     labeled_vars_in_df   <- sort(colnames(df)[ (colnames(df) %in% vals_df$variable) ])
     unlabeled_vars_in_df <- sort(colnames(df)[!(colnames(df) %in% vals_df$variable) ])
 
-    message("\n Labeled vars: \n")
-    message(paste(labeled_vars_in_df, collapse=', '), '\n')
-    message("\n Unlabeled vars: \n")
-    message(paste(unlabeled_vars_in_df, collapse=', '), '\n')
+    message("Labeled vars: ")
+    message(paste(labeled_vars_in_df, collapse=', '))
+    message("Unlabeled vars: ")
+    message(paste(unlabeled_vars_in_df, collapse=', '))
   }
   if ( df_is_dt ){
      df_labeled = data.table::data.table(df_labeled)
