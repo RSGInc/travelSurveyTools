01a874188d9c1004fe6d3103e2902ab938911a01
Ashley Asmus <ashley.asmus@rsginc.com>
2026-03-13 08:50:08 -0500

    add test

--- tests/testthat/test_hts_validate_variable_list.R
+++ tests/testthat/test_hts_validate_variable_list.R
@@ -19,3 +19,15 @@ test_that("hts_validate_variable_list should return a warning", {
 
   expect_warning(hts_validate_variable_list(variable_list, test_data))
 })
+
+test_that("hts_validate_variable_list preserves extra metadata columns", {
+  data("variable_list")
+
+  variable_list[, logic := NA_character_]
+  variable_list[variable == "age", logic := "Asked of all persons"]
+
+  results = hts_validate_variable_list(variable_list, test_data)
+
+  expect_true("logic" %in% names(results))
+  expect_equal(results[variable == "age", logic], "Asked of all persons")
+})
